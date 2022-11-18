8ebf843790279698b4dd609d26e88988ff258270
Lucas Marthins <marthins.1570@gmail.com>
2022-11-17 22:18:33 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <h1 className='text-slate-900 text-4xl font-sans'>Hello world</h1>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: 'Roboto, sans-serif'
+        },
+      },
+    },
+    plugins: [],
+  }
